9edcb4dbb10086d8d569c54fea3af2316c77c17b
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-23 10:21:42 -0400

    checkpoint

---
+++ demo-2-5b.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var firstNum = 7;
+
+
+var toThePowerof = 2;
+
+
+var answer = Math.pow(firstNum, toThePowerof);
+
+console.log(answer);
+
+console.log(Math.pow(7,2));
+
+
+console.log(7 ** 2);
+
+
+
